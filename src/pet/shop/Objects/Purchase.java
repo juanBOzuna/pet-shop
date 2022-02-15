ca5836a0447db5fa0a64a9756a4a973e3eebebd5
juanBOzuna <70336894+juanBOzuna@users.noreply.github.com>
2022-02-15 08:25:53 -0500

    init petShop

---
+++ src/pet/shop/Objects/Purchase.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pet.shop.Objects;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author juan barraza
+ */
+public class Purchase {
+    
+    int idCompra;
+    double total_price;
+    Calendar date;
+    
+//    Date
+    
+    
+}
